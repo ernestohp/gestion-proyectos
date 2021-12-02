89aacb465135a1f89d71fbcacf7e5d529936e9f6
ernestohp <34515723+ernestohp@users.noreply.github.com>
2021-12-02 21:46:20 +0100

    Add files via upload

--- proyectos-front/src/componentes/proyectos/Constantes.js
+++ proyectos-front/src/componentes/proyectos/Constantes.js
@@ -1,1 +1,5 @@
+//--Plantilla por defecto---
 export const DAFAULT_TEMPLATE = "IVS"
+
+//--Tareas que se envían a OPP sí o sí---
+export const TAREAS_DEFECTO_OPP = ["Project Owner", "Account Manager", "Reunión Equipo", "Reunión Cliente"]
